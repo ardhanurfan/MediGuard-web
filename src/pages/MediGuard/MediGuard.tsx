a78f3668422c81e518cbce538b08dcf7322866b3
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2023-11-19 03:08:30 +0700

    feat: api get on going mediguard

--- src/pages/MediGuard/MediGuard.tsx
+++ src/pages/MediGuard/MediGuard.tsx
@@ -1,133 +1,81 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
+import { getWithAuth } from "../../api/api";
+import Cookies from "js-cookie";
+import { toastError } from "../../components/Toast/Toast";
 
-const dataDummy = [
-  {
-    id: 1,
-    mediGuardCode: "MD000000-01",
-    plateNumber: "AB 3449 TT",
-    departureTime: "16 August 2023, 12.35",
-    temperature: "40 ºC",
-    humidity: "35 %",
-    progress: "Delivering PCK0000000-01",
-    transaction: [
-      {
-        transactionCode: "PCK000000-01",
-        address: "Jln. Lorem Ipsum No. Dolor, Sit, Amet 000000",
-        distance: "24km",
-        timeEstimation: "01:00",
-        status: "Safe",
-        onGoing: false,
-        product: [
-          {
-            productName: "Paracetamol 200mg",
-            type: "Tablet",
-            amount: "200",
-          },
-        ],
-      },
-      {
-        transactionCode: "PCK000000-01",
-        address: "Jln. Lorem Ipsum No. Dolor, Sit, Amet 000000",
-        distance: "24km",
-        timeEstimation: "01:00",
-        status: "Safe",
-        onGoing: true,
-        product: [
-          {
-            productName: "Paracetamol 200mg",
-            type: "Tablet",
-            amount: "200",
-          },
-        ],
-      },
-      {
-        transactionCode: "PCK000000-01",
-        address: "Jln. Lorem Ipsum No. Dolor, Sit, Amet 000000",
-        distance: "24km",
-        timeEstimation: "01:00",
-        status: "Safe",
-        onGoing: false,
-        product: [
-          {
-            productName: "Paracetamol 200mg",
-            type: "Tablet",
-            amount: "200",
-          },
-        ],
-      },
-    ],
-  },
-  {
-    id: 1,
-    mediGuardCode: "MD000000-02",
-    plateNumber: "AB 3449 TT",
-    departureTime: "16 August 2023, 12.35",
-    temperature: "40 ºC",
-    humidity: "35 %",
-    progress: "Delivering PCK0000000-01",
-    transaction: [
-      {
-        transactionCode: "PCK000000-01",
-        address: "Jln. Lorem Ipsum No. Dolor, Sit, Amet 000000",
-        distance: "24km",
-        timeEstimation: "01:00",
-        status: "Safe",
-        onGoing: false,
-        product: [
-          {
-            productName: "Paracetamol 200mg",
-            type: "Tablet",
-            amount: "200",
-          },
-        ],
-      },
-      {
-        transactionCode: "PCK000000-01",
-        address: "Jln. Lorem Ipsum No. Dolor, Sit, Amet 000000",
-        distance: "24km",
-        timeEstimation: "01:00",
-        status: "Safe",
-        onGoing: true,
-        product: [
-          {
-            productName: "Paracetamol 200mg",
-            type: "Tablet",
-            amount: "200",
-          },
-        ],
-      },
-      {
-        transactionCode: "PCK000000-01",
-        address: "Jln. Lorem Ipsum No. Dolor, Sit, Amet 000000",
-        distance: "24km",
-        timeEstimation: "01:00",
-        status: "Safe",
-        onGoing: false,
-        product: [
-          {
-            productName: "Paracetamol 200mg",
-            type: "Tablet",
-            amount: "200",
-          },
-        ],
-      },
-    ],
-  },
-];
+interface ProductDetail {
+  _id: string;
+  prod_code: string;
+  hna: number;
+  het: number;
+  uom: string;
+  beratGram: number;
+  panjangCm: number;
+  lebarCm: number;
+  tinggiCm: number;
+  isLifeSaving: boolean;
+  volumeCm3: number;
+  kategoriPengiriman: number;
+}
+
+interface Address {
+  alamat: string;
+}
+
+interface DeliveryCat {
+  kateogriPengiriman: string;
+  suhuSimpan: string;
+  minSuhu: number;
+  maxSuhu: number;
+}
+
+interface TransactionUnit {
+  orderNum: number;
+  productDetails: ProductDetail[];
+  address: Address;
+  distance: number | null;
+  timeEstimation: number | null;
+  deliveryCat: DeliveryCat;
+}
+
+interface Unit {
+  unitId: string;
+  batteryCapacity: number;
+  temperature: number;
+  humidity: number;
+  platNumber: string | null;
+  departureTime: string | null;
+  currentTransaction: number | null;
+  transactionUnits: TransactionUnit[];
+}
 
 function MediGuard() {
-  const [detailProduct, setDetailProduct] = useState<
-    {
-      productName: string;
-      type: string;
-      amount: string;
-    }[]
-  >([]);
+  const [detailProduct, setDetailProduct] = useState<ProductDetail[]>([]);
 
   const [showDetailTransaction, setShowDetailTransaction] = useState<
     string | null
   >(null);
+
+  const [mediGuards, setMediGuards] = useState<Unit[]>([]);
+
+  const token = Cookies.get("token_mediguard");
+  const getOnGoing = async () => {
+    if (token) {
+      try {
+        const response = await getWithAuth(token, "unit/get-on-going");
+        const data = response.data?.data as Unit[];
+        setMediGuards(data);
+        console.log(mediGuards);
+      } catch (error) {
+        toastError("Get MediGuards Failed");
+      }
+    }
+  };
+
+  useEffect(() => {
+    getOnGoing();
+  }, []);
 
   return (
     <>
@@ -143,49 +91,57 @@ function MediGuard() {
               <div className="flex h-auto w-auto border-collapse bg-kHeadTable py-1 text-center font-medium text-[12px] text-kGrey">
                 <div className="w-[5%]"></div>
                 <div className="w-[15%]">MediGuard Code</div>
-                <div className="w-[15%]">Plate Number</div>
+                <div className="w-[10%]">Plate Number</div>
                 <div className="w-[20%]">Departure Time</div>
                 <div className="w-[10%]">Temperature</div>
                 <div className="w-[10%]">Humidity</div>
-                <div className="w-[25%]">Progress</div>
+                <div className="w-[10%]">Baterai</div>
+                <div className="w-[20%]">Progress</div>
               </div>
-              {dataDummy.map((data) => {
+              {mediGuards.map((data) => {
                 return (
-                  <div key={data.id}>
+                  <div key={data.unitId}>
                     <div className="flex h-auto w-auto border-collapse py-3 text-center text-[12px] text-kGrey">
                       <div
                         onClick={() => {
-                          if (data.mediGuardCode == showDetailTransaction) {
+                          if (data.unitId == showDetailTransaction) {
                             setShowDetailTransaction(null);
                           } else {
-                            setShowDetailTransaction(data.mediGuardCode);
+                            setShowDetailTransaction(data.unitId);
                           }
                         }}
                         className="w-[5%] text-[24px] text-kBlue-400 flex justify-center items-center cursor-pointer hover:text-kBlue-200 active:text-kBlue-300"
                       >
-                        {data.mediGuardCode == showDetailTransaction ? (
+                        {data.unitId == showDetailTransaction ? (
                           <IoIosArrowUp />
                         ) : (
                           <IoIosArrowDown />
                         )}
                       </div>
-                      <div className="w-[15%] text-kBlue-400 font-bold">
-                        {data.mediGuardCode}
+                      <div className="w-[15%] text-kBlue-400 font-bold flex justify-center items-center">
+                        {data.unitId}
                       </div>
-                      <div className="w-[15%]">{data.plateNumber}</div>
-                      <div className="w-[20%]">{data.departureTime}</div>
-                      <div className="w-[10%] font-semibold text-kBlue-400">
+                      <div className="w-[10%] flex justify-center items-center">
+                        {data.platNumber}
+                      </div>
+                      <div className="w-[20%] flex justify-center items-center">
+                        {data.departureTime}
+                      </div>
+                      <div className="w-[10%] font-semibold text-kBlue-400 flex justify-center items-center">
                         {data.temperature}
                       </div>
-                      <div className="w-[10%] font-semibold text-kBlue-400">
+                      <div className="w-[10%] font-semibold text-kBlue-400 flex justify-center items-center">
                         {data.humidity}
                       </div>
-                      <div className="w-[25%] text-kBlue-400 font-bold">
-                        {data.progress}
+                      <div className="w-[10%] font-semibold text-kBlue-400 flex justify-center items-center">
+                        {data.batteryCapacity}
+                      </div>
+                      <div className="w-[20%] text-kBlue-400 font-bold flex justify-center items-center">
+                        {"Delivering " + data.currentTransaction}
                       </div>
                     </div>
-                    {showDetailTransaction == data.mediGuardCode && (
-                      <div className="w-full bg-kBlue-100 bg-opacity-50">
+                    {showDetailTransaction == data.unitId && (
+                      <div className="w-full bg-kBlue-100 bg-opacity-30">
                         <div className="flex h-auto w-auto border-collapse py-3 text-center text-[12px] text-kBlue-400 font-bold">
                           <div className="w-[5%]"></div>
                           <div className="w-[15%]">Transaction Code</div>
@@ -195,36 +151,52 @@ function MediGuard() {
                           <div className="w-[15%]">Status</div>
                           <div className="w-[15%]">Detail</div>
                         </div>
-                        {data.transaction.map((transactionData) => {
+                        {data.transactionUnits.map((transaction) => {
                           return (
                             <div
-                              key={transactionData.transactionCode}
+                              key={transaction.orderNum}
                               className={`flex h-auto w-auto border-collapse py-3 text-center text-[12px] ${
-                                transactionData.onGoing
-                                  ? "text-kBlue-400 font-medium"
+                                transaction.orderNum == data.currentTransaction
+                                  ? "text-kBlue-400 font-bold"
                                   : "text-kGrey"
                               }`}
                             >
                               <div className="w-[5%]"></div>
                               <div className="w-[15%]">
-                                {transactionData.transactionCode}
+                                {transaction.orderNum}
                               </div>
                               <div className="w-[25%]">
-                                {transactionData.address}
+                                {transaction.address.alamat}
                               </div>
                               <div className="w-[15%]">
-                                {transactionData.distance}
+                                {transaction.distance}
                               </div>
                               <div className="w-[15%]">
-                                {transactionData.timeEstimation}
+                                {transaction.timeEstimation}
                               </div>
-                              <div className="w-[15%]">
-                                {transactionData.status}
+                              <div
+                                className={`w-[15%] ${
+                                  data.temperature >
+                                  transaction.deliveryCat.maxSuhu
+                                    ? "text-kRed"
+                                    : data.temperature <
+                                      transaction.deliveryCat.minSuhu
+                                    ? "text-kRed"
+                                    : "text-kGreen-200"
+                                }`}
+                              >
+                                {data.temperature >
+                                transaction.deliveryCat.maxSuhu
+                                  ? "Over Temperature"
+                                  : data.temperature <
+                                    transaction.deliveryCat.minSuhu
+                                  ? "Under Temperature"
+                                  : "Safe"}
                               </div>
                               <div className="w-[15%] flex justify-center items-center">
                                 <button
                                   onClick={() =>
-                                    setDetailProduct(transactionData.product)
+                                    setDetailProduct(transaction.productDetails)
                                   }
                                   className="text-[12px] border-2 border-kBlue-200 text-kBlue-200 font-medium rounded-lg px-5 py-1 hover:text-white hover:bg-kBlue-200 active:bg-kBlue-300"
                                 >
